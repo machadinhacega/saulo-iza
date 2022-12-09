89201459a1655788b211113747139b724b937974
Saulo Cruz <saulovcruz@gmail.com>
2022-12-09 10:39:43 -0300

    le o json da planilha e atualiza a pagina

--- assets/scripts/lista-presentes.js
+++ assets/scripts/lista-presentes.js
@@ -1,6 +1,40 @@
-import listaPresentes from './lista.json' assert {type: 'json'};
+let url = 'https://sheets.googleapis.com/v4/spreadsheets/1m69ur2MbXpsb9T-FarE0QWvy5PCPpgc66YQZOZc_OEI/values/lista-presentes?alt=json&key=AIzaSyAdHQK3RJJ3hZyA5f6kuM-y6Bv4nLufIpg';
 
-function carregarPresentes() {
+async function fetchListaJson() {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+
+  const listaJson = await response.json();
+  return listaJson;
+}
+
+function trataLista(lista) {
+  var vetores = lista.values;
+  var chaves = lista.values[0];
+
+  var listaPresentes = [];
+
+  for ( let i=1; i <vetores.length; i++) {
+      var presente = vetores[i];
+      var x = 0;
+      var novoPresente = {};
+      presente.forEach(el => {
+          novoPresente[chaves[x]] = el;
+          x++;
+      });
+      listaPresentes.push(novoPresente);
+  };
+
+  var listaPresentesJson = JSON.parse(JSON.stringify(listaPresentes));
+  return listaPresentesJson;
+}
+
+
+function carregarPresentes(listaPresentes) {
     let presentes_container = document.querySelector(".lista-presentes");
         presentes_container.innerHTML = "";
 
@@ -20,4 +54,8 @@ function carregarPresentes() {
     });
 }
 
-carregarPresentes();
+
+fetchListaJson().then(listaJson => {
+  let listaPresentes = trataLista(listaJson);
+  carregarPresentes(listaPresentes);
+})
